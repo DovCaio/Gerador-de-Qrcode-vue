6683bf127c1ff4a6200c9c0d7d8f4c88389d00d3
DovCaio <onlydeadnotalive@gmail.com>
2024-08-03 20:26:57 -0300

    Mexido nos diretorios do vue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
+import Button from "./components/Button.vue"
+</script>
+
+<template>
+  <header>
+    
+
+    <div class="wrapper">
+      
+      
+      
+    </div>
+    </header>
+    
+  <main>
+
+    <Button val="dadea"></Button>
+
+
+  </main>
+
+</template>
+
+<style scoped>
+
+</style>
